01817929391fe0f3af9a99bc14820edd194d24f5
Piet Geursen <pietgeursen@gmail.com>
2016-02-14 01:02:33 +1300

    Add Products h4 and fix id

--- app/components/product-list.js
+++ app/components/product-list.js
@@ -5,6 +5,9 @@ function render(state) {
   const productSummaries = state.props.map( product => 
     h(ProductSummary, product)
   )
-  return h('div',{class: 'products'}, productSummaries) 
+  return h('div',{id: 'productList'}, [
+		h('h4', {textContent: 'Products'}),
+		productSummaries,
+	]) 
 }
 export default render
